48c07ca9eec16ec9bedad81d307d0c908079d2a9
AaronReyas <hsayer87@gmail.com>
2021-12-10 14:05:43 +0000

    added all the tables to models.py and formed relationships between them. Users now has capabilities for user logins

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from app import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'User'
     email = db.Column(db.String(100), primary_key=True)
     role = db.Column(db.String(10), nullable=False)
@@ -10,6 +11,11 @@ class User(db.Model):
     firstName = db.Column(db.String(30), nullable=False)
     surname = db.Column(db.String(30), nullable=False)
     UID = db.Column(db.String(36), nullable=False)
+
+    # relationships to other tables
+    engages = db.relationship('Engage')
+    takes = db.relationship('Take')
+    creates = db.relationship('Create')
 
     def __init__(self, email, role, password, schoolID, firstName, surname, UID):
         self.email = email
@@ -26,11 +32,34 @@ class School(db.Model):
     ID = db.Column(db.String(15), primary_key=True)
     schoolName = db.Column(db.String(100), nullable=False)
 
+    # relationships to other tables
     users = db.relationship('User')
 
     def __init__(self, ID, schoolName):
         self.ID = ID
         self.schoolName = schoolName
+
+
+class Course(db.Model):
+    __tablename__ = 'Course'
+    CID = db.Column(db.String(15), primary_key=True)
+    courseName = db.Column(db.String(100), nullable=False)
+
+    engages = db.relationship('Engage')
+
+    def __init__(self, CID, courseName):
+        self.CID = CID
+        self.courseName = courseName
+
+
+class Engage(db.Model):
+    __tablename__ = 'Engage'
+    email = db.Column(db.String(100), primary_key=True)
+    CID = db.Column(db.String(15), primary_key=True)
+
+    def __init__(self, email, CID):
+        self.email = email
+        self.CID = CID
 
 
 class Assignment(db.Model):
@@ -40,8 +69,38 @@ class Assignment(db.Model):
     description = db.Column(db.String(100), nullable=False)
     deadline = db.Column(db.String(10), nullable=False)
 
+    # relationships to other tables
+    takes = db.relationship('Take')
+    creates = db.relationship('Create')
+
     def __init__(self, AID, assignmentName, description, deadline):
         self.AID = AID
         self.assignmentName = assignmentName
         self.description = description
         self.deadline = deadline
+
+
+class Take(db.Model):
+    __tablename__ = 'Take'
+    email = db.Column(db.String(100), primary_key=True)
+    AID = db.Column(db.String(15), primary_key=True)
+    submitTime = db.Column(db.DateTime, nullable=False)
+    grade = db.Column(db.Float, nullable=True)
+
+    def __init__(self, email, AID, submitTime, grade):
+        self.email = email
+        self.AID = AID
+        self.submitTime = submitTime
+        self.grade = grade
+
+
+class Create(db.Model):
+    __tablename__ = 'Create'
+    email = db.Column(db.String(100), primary_key=True)
+    AID = db.Column(db.String(15), primary_key=True)
+    CreateTime = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, email, AID, createTime):
+        self.email = email
+        self.AID = AID
+        self.createTime = createTime
