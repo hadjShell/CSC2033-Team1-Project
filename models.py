beb4b0793b6f897cd125727eeb11da9958ceb49c
AaronReyas <hsayer87@gmail.com>
2021-12-10 13:01:44 +0000

    added User and School to models.py so that the user login/register functionality can be done

--- models.py
+++ models.py
@@ -1,6 +1,38 @@
 from app import db
 
 
+class User(db.Model):
+    __tablename__ = 'User'
+    email = db.Column(db.String(100), primary_key=True)
+    role = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    schoolID = db.Column(db.String(15), nullable=False)
+    firstName = db.Column(db.String(30), nullable=False)
+    surname = db.Column(db.String(30), nullable=False)
+    UID = db.Column(db.String(36), nullable=False)
+
+    def __init__(self, email, role, password, schoolID, firstName, surname, UID):
+        self.email = email
+        self.role = role
+        self.password = password
+        self.schoolID = schoolID
+        self.firstName = firstName
+        self.surname = surname
+        self.UID = UID
+
+
+class School(db.Model):
+    __tablename__ = 'School'
+    ID = db.Column(db.String(15), primary_key=True)
+    schoolName = db.Column(db.String(100), nullable=False)
+
+    users = db.relationship('User')
+
+    def __init__(self, ID, schoolName):
+        self.ID = ID
+        self.schoolName = schoolName
+
+
 class Assignment(db.Model):
     __tablename__ = 'Assignment'
     AID = db.Column(db.String(15), primary_key=True)
