7ec277d94f5a4b67c5da94a00c4b7579bf998d34
uzairyousaf <u.yousaf1@newcastle.ac.uk>
2021-12-14 17:45:00 +0000

    Created Register flask form to give register form working features. Also added error messaging in register.html for when fields aren't filled in to the preset standards

--- users/views.py
+++ users/views.py
@@ -1,10 +1,50 @@
-from flask import Blueprint, render_template, flash, redirect
+from flask import Blueprint, render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
+import logging
 from app import db
 from models import User
-from users.forms import LoginForm
+from users.forms import LoginForm, RegisterForm
 
-users_blueprint = Blueprint('users', name, template_folder='templates')
+users_blueprint = Blueprint('users', __name__, template_folder='templates')
+
+
+# VIEWS
+# view registration
+
+
+@users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    # create signup form object
+    form = RegisterForm()
+
+    # if request method is POST or form is valid
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        # if this returns a user, then the email already exists in database
+
+        # if email already exists redirect user back to signup page with error message so user can try again
+        if user:
+            flash('Email address already exists')
+            return render_template('register.html', form=form)
+
+        # create a new user with the form data
+        new_user = User(email=form.email.data,
+                        firstName=form.firstName.data,
+                        surname=form.lastName.data,
+                        password=form.password.data,
+                        #                        pin_key=form.pin_key.data,
+                        role='user')
+
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        logging.warning('SECURITY - User registration [%s, %s]', form.email.data, request.remote_addr)
+
+        # sends user to login page
+        return redirect((url_for('users.login')))
+    # if request method is GET or form not valid re-render signup page
+    return render_template('register.html', form=form)
 
 
 @users_blueprint.route('/login', methods=['GET', 'POST'])
