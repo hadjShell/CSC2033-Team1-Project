f86a21ed54415618dea3dc403afee05529962d73
Jiayuan Zhang <c0057833@ncl.ac.uk>
2022-01-01 10:05:07 +0000

    Create class list view function

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 # IMPORT
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_required
 from app import db
 from courses.forms import CourseForm
@@ -21,6 +21,28 @@ def courses():
 
     # render course page with engaged courses
     return render_template('course.html', engaged_courses=engaged_courses)
+
+
+# View the class list of a specific course
+@courses_blueprint.route('/courses/classlist', methods=['GET', 'POST'])
+def course_classlist():
+    # get course id
+    if request.method == 'POST':
+        course_id = request.form.get('class_list')
+
+    # get course name
+    course_name = Course.query.filter_by(CID=course_id).first().courseName
+
+    # get class list
+    class_list = []
+    engaged = Engage.query.filter_by(CID=course_id).all()
+    for e in engaged:
+        class_list.append(User.query.filter_by(email=e.email).first())
+    for s in class_list:
+        if s.role == "teacher":
+            class_list.remove(s)
+
+    return render_template('course-classlist.html', course_id=course_id, course_name=course_name, class_list=class_list)
 
 
 '''
